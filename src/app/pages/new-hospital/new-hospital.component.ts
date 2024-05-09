e671579a85799ef39f2e87c3efca6d1ab34a7e29
Bishnu Behera <bishnu.glintlets@gmail.com>
2024-05-09 17:16:43 +0530

    doctor appointment app

---
+++ src/app/pages/new-hospital/new-hospital.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-hospital',
+  standalone: true,
+  imports: [],
+  templateUrl: './new-hospital.component.html',
+  styleUrl: './new-hospital.component.scss'
+})
+export class NewHospitalComponent {
+
+}
